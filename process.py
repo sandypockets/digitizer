b57782bda01bc674160884e447dc1709a953b365
sandypockets <zac@znacreative.com>
2021-09-21 15:23:11 -0400

    Add more image processing functions

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 import glob
 import shutil
 import os
-from PIL import Image
+from PIL import Image, ImageOps, ImageEnhance
 
 
 class Digitizer:
@@ -12,6 +12,19 @@ class Digitizer:
     def make_upside_down(self):
         print("Make upside down")
         self.img = self.img.rotate(180)
+
+    def make_thumbnail_size(self, size=(128, 128)):
+        self.img.thumbnail(size)
+
+    def make_square(self, size=200):
+        self.img = self.img.resize((size, size))
+
+    def make_grayscale(self):
+        self.img = ImageOps.grayscale(self.img)
+
+    def adjust_contrast(self, amount=1.5):
+        enhancer = ImageEnhance.Contrast(self.img)
+        self.img = enhancer.enhance(amount)
 
     def save(self, output_filepath):
         self.img.save(output_filepath)
@@ -24,6 +37,8 @@ os.makedirs("outputs", exist_ok=True)
 for filepath in inputs:
     output = filepath.replace("inputs", "outputs")
     image = Digitizer(filepath)
+    image.make_thumbnail_size((200, 200))
+    image.make_grayscale()
     image.make_upside_down()
     image.save(output)
 
