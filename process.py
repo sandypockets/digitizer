4c45407712f4e5ee5a37fe4e09f33306f8ffd9eb
sandypockets <zac@znacreative.com>
2021-09-21 15:44:40 -0400

    Add watermark, add/remove alpha channel on images during processing

--- process.py
+++ process.py
@@ -1,13 +1,13 @@
 import glob
 import shutil
 import os
-from PIL import Image, ImageOps, ImageEnhance
+from PIL import Image, ImageOps, ImageEnhance, ImageDraw, ImageFont
 
 
 class Digitizer:
     def __init__(self, filepath):
         self.filepath = filepath
-        self.img = Image.open(filepath)
+        self.img = Image.open(filepath).convert("RGBA")
 
     def make_upside_down(self):
         self.img = self.img.rotate(180)
@@ -27,14 +27,23 @@ class Digitizer:
             box = (x, y, w + x, w + y)
         self.img = self.img.resize((size, size), box=box)
 
+    def add_watermark(self):
+        font = ImageFont.truetype("fonts/ibm-plex-mono.ttf", 24)
+        drawer = ImageDraw.Draw(self.img)
+        drawer.text((32, 32), "sandypockets", font=font, fill=(255, 0, 0))
+
     def make_grayscale(self):
         self.img = ImageOps.grayscale(self.img)
+        self.img = self.img.convert("RGBA")
 
     def adjust_contrast(self, amount=1.5):
         enhancer = ImageEnhance.Contrast(self.img)
         self.img = enhancer.enhance(amount)
 
     def save(self, output_filepath):
+        if self.filepath.endswith(".jpg"):
+            self.img = self.img.convert("RGB")
+
         self.img.save(output_filepath)
 
 
@@ -45,9 +54,9 @@ os.makedirs("outputs", exist_ok=True)
 for filepath in inputs:
     output = filepath.replace("inputs", "outputs")
     image = Digitizer(filepath)
-    image.make_thumbnail_size((200, 200))
-    image.make_square()
+    image.adjust_contrast()
     image.make_grayscale()
-    image.make_upside_down()
+    image.make_square()
+    image.add_watermark()
     image.save(output)
 
